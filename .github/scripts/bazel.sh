4a1d494490d52f9668aa3d0e4a2eddee03704e11
David Freese <freese@verily.com>
2021-02-03 07:22:13 -0800

    Intial attempt for bazel build

---
+++ .github/scripts/bazel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eux
+
+"${GITHUB_WORKSPACE}/bin/bazel" build //...
+"${GITHUB_WORKSPACE}/bin/bazel" test //...
+
+# "${GITHUB_WORKSPACE}/bin/bazel" run //tools:examples_raze
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo '/examples is out of date. Please rerun all tests and commit the changes generated from this command' >&2
+  exit 1
+fi
+
+# Run Buildifier as a linter step
+"${GITHUB_WORKSPACE}/bin/bazel" run //:buildifier_check
